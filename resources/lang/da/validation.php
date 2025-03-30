8a8401a61846965c6e6774010f21f013fa2c7c04
Lui Thorsen <laht93@gmail.com>
2025-03-30 05:05:14 +0200

    another error message added

--- resources/lang/da/validation.php
+++ resources/lang/da/validation.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+	'unique' => 'Denne email er allerede i brug.',
 	'confirmed' => 'Kodeordene er ikke ens. Prøv igen.',
 	'min' => [
 		'string' => 'Dit kodeord skal være mindst 8 tegn.',
